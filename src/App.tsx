cf8a4786bda1b4c57a00b75390fa6bac0bb835b6
Dracule Mihawk <105357696+takaNoMeImer@users.noreply.github.com>
2022-12-15 21:57:03 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
-
-export default App
-=======
 import Content from "./components/Content";
 import Information from "./components/Infomation";
 import Navbar from "./components/Navbar";
@@ -53,4 +17,3 @@ const App = () => {
 }
 
 export default App;
->>>>>>> 630f474 (feat: Before Cart)
